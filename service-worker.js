32e99a19e6055475a76d4c51a3c9f6f9e9158051
Lyneloflight <110337686+Lyneloflight@users.noreply.github.com>
2025-05-02 01:40:52 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'my-cache-v1';
+const CACHE_FILES = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/service-worker.js',
+  '/icons/icon-15x15.jpeg',
+  '/icons/icon-16x16.jpeg',
+  '/icons/icon-32x32.jpeg',
+  '/icons/icon-132x132.jpeg',
+  '/icons/icon-158x158.jpeg',
+  '/game.js',
+  '/gamelib.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHE_FILES);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
